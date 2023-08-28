ad1c5bf8b23a4d3a6e91b0f1e7850bb7de491c3a
jjonespfit <jordan.jones@pathforwardit.com>
2023-08-28 09:52:29 -0500

    commit

--- VMwareReports/Update Vmware Email Address - PowerCLI.ps1
+++ VMwareReports/Update Vmware Email Address - PowerCLI.ps1
@@ -4,9 +4,9 @@ $newEmail = 'pfitalerts@pathforwardit.com'
 foreach ($alarm in Get-AlarmDefinition){
     $action = Get-AlarmAction -AlarmDefinition $alarm
     $mail = $action | Where-Object {$_.ActionType -eq 'SendEmail' -and $_.To -contains $oldEmail}
+    $trigger = Get-AlarmActionTrigger -AlarmAction $mail
 
     Remove-AlarmAction -AlarmAction $mail -Confirm:$false
-    New-AlarmAction -AlarmDefinition $alarm -Email -To $newEmail -Subject $mail.Subject -Confirm:$false
+    New-AlarmAction -AlarmDefinition $alarm -Email -To $newEmail -Subject $mail.Subject -AlarmActionTrigger $trigger -Confirm:$false
 
 }
-
